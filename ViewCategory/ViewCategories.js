81a3f07b2000e2222c6d9a8125e48b57fdad7fad
justomar21 <tarekes68@gmail.com>
2024-01-07 03:24:57 +0200

    Removed the operation of removing a product from Orders list if the product is deleted

--- ViewCategory/ViewCategories.js
+++ ViewCategory/ViewCategories.js
@@ -4,117 +4,99 @@ let table = document.querySelector("table");
 retrieveData();
 
 function retrieveData() {
-  let categories = localStorage.getItem("category");
-  categories = categories ? JSON.parse(categories) : [];
-  for (let cat of categories) {
-    codeBuilderTR(cat);
-  }
+   let categories = localStorage.getItem("category");
+   categories = categories ? JSON.parse(categories) : [];
+   for (let cat of categories) {
+      codeBuilderTR(cat);
+   }
 }
 
 function codeBuilderTR(cat) {
-  let IDTD = document.createElement("td");
-  IDTD.innerHTML = cat.id;
+   let IDTD = document.createElement("td");
+   IDTD.innerHTML = cat.id;
 
-  let titleTD = document.createElement("td");
-  titleTD.innerHTML = cat.name;
+   let titleTD = document.createElement("td");
+   titleTD.innerHTML = cat.name;
 
-  let descTD = document.createElement("td");
-  descTD.innerHTML = cat.description;
+   let descTD = document.createElement("td");
+   descTD.innerHTML = cat.description;
 
-  let operationTD = document.createElement("td");
+   let operationTD = document.createElement("td");
 
-  let editBtn = document.createElement("button");
-  editBtn.classList.add("edit");
-  editBtn.addEventListener("click" , updateCategory);
-  editBtn.innerHTML = "Edit";
+   let editBtn = document.createElement("button");
+   editBtn.classList.add("edit");
+   editBtn.addEventListener("click", updateCategory);
+   editBtn.innerHTML = "Edit";
 
-  let deleteBtn = document.createElement("button");
-  deleteBtn.classList.add("delete");
-  deleteBtn.addEventListener("click", removeCategory);
-  deleteBtn.innerHTML = "Delete";
+   let deleteBtn = document.createElement("button");
+   deleteBtn.classList.add("delete");
+   deleteBtn.addEventListener("click", removeCategory);
+   deleteBtn.innerHTML = "Delete";
 
-  operationTD.appendChild(editBtn);
-  operationTD.appendChild(document.createTextNode(" "));
-  operationTD.appendChild(deleteBtn);
+   operationTD.appendChild(editBtn);
+   operationTD.appendChild(document.createTextNode(" "));
+   operationTD.appendChild(deleteBtn);
 
-  let TR = document.createElement("tr");
-  TR.appendChild(IDTD);
-  TR.appendChild(titleTD);
-  TR.appendChild(descTD);
-  TR.appendChild(operationTD);
+   let TR = document.createElement("tr");
+   TR.appendChild(IDTD);
+   TR.appendChild(titleTD);
+   TR.appendChild(descTD);
+   TR.appendChild(operationTD);
 
-  table.firstElementChild.appendChild(TR);
+   table.firstElementChild.appendChild(TR);
 }
 
 function categoryNumberCheck() {
-  const categoryNumber = document.querySelector(".categoryNumber");
-  let categories = localStorage.getItem("category");
-  categories = categories ? JSON.parse(categories) : [];
-  categoryNumber.innerText = categories.length;
+   const categoryNumber = document.querySelector(".categoryNumber");
+   let categories = localStorage.getItem("category");
+   categories = categories ? JSON.parse(categories) : [];
+   categoryNumber.innerText = categories.length;
 }
 
-function updateCategory(e)
-{
-  let catID = e.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.innerText;
-  localStorage.setItem("updateCatID",catID);
-  window.location.assign("../UpdateCategories/updateCategories.html");
+function updateCategory(e) {
+   let catID = e.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.innerText;
+   localStorage.setItem("updateCatID", catID);
+   window.location.assign("../UpdateCategories/updateCategories.html");
 }
 
-function removeCategory(e) 
-{
-  let catID = e.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.innerText;
-  e.target.parentElement.parentElement.remove();
-  let categories = localStorage.getItem("category");
-  categories = categories ? JSON.parse(categories) : [];
+function removeCategory(e) {
+   let catID = e.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.innerText;
+   e.target.parentElement.parentElement.remove();
+   let categories = localStorage.getItem("category");
+   categories = categories ? JSON.parse(categories) : [];
 
-  for (let singleCat in categories) {
-    if (categories[singleCat].id == catID) {
-      removeProduct(categories[singleCat]);
+   for (let singleCat in categories) {
+      if (categories[singleCat].id == catID) {
+         removeProduct(categories[singleCat]);
+         categories.splice(singleCat, 1);
+      }
 
-      categories.splice(singleCat, 1);
-    }
-
-  }
-  localStorage.setItem("category", JSON.stringify(categories));
-  categoryNumberCheck();
+   }
+   localStorage.setItem("category", JSON.stringify(categories));
+   categoryNumberCheck();
 }
 
-function removeProduct(singleCat) 
-{
-  let products = localStorage.getItem("product");
-  products = products ? JSON.parse(products) : [];
-  for (let singleProduct in products) {
-    if (singleCat.id == products[singleProduct].categoryID) {
-      removeOrder(products[singleProduct]);
-      removeWish(products[singleProduct]);
-      products.splice(singleProduct, 1);
-    }
-  }
-  localStorage.setItem("product", JSON.stringify(products));
+function removeProduct(singleCat) {
+   let products = localStorage.getItem("product");
+   products = products ? JSON.parse(products) : [];
+   for (let singleProduct in products) {
+      if (singleCat.id == products[singleProduct].categoryID) {
+         removeWish(products[singleProduct]);
+         products.splice(singleProduct, 1);
+      }
+   }
+   localStorage.setItem("product", JSON.stringify(products));
 }
 
-function removeOrder(singleProduct) 
-{
-  let orders = localStorage.getItem("order");
-  orders = orders ? JSON.parse(orders) : [];
-  for (let singleOrder in orders) {
-    if (singleProduct.id == orders[singleOrder].productID) {
-      orders.splice(singleOrder, 1);
-    }
-  }
-  localStorage.setItem("order", JSON.stringify(orders));
-}
-
-function removeWish(singleProduct) 
-{
-  let wishlists = localStorage.getItem("wishlist");
-  wishlists = wishlists ? JSON.parse(wishlists) : [];
-  for (let singleWish in wishlists) {
-    if (singleProduct.id == wishlists[singleWish].productID) {
-      wishlists.splice(singleWish, 1);
-    }
-  }
-  localStorage.setItem("wishlist", JSON.stringify(wishlists));
+function removeWish(singleProduct) {
+   let wishlists = localStorage.getItem("wishlist");
+   wishlists = wishlists ? JSON.parse(wishlists) : [];
+   for (let singleWish in wishlists) {
+      if (singleProduct.id == wishlists[singleWish].productID) {
+         wishlists.splice(singleWish, 1);
+      }
+   }
+   localStorage.setItem("wishlist", JSON.stringify(wishlists));
 }
 
 categoryNumberCheck();
@@ -124,43 +106,43 @@ categoryNumberCheck();
 // #region Local Storage Classes
 
 class Category {
-  constructor(id, name, description) {
-    this.id = id;
-    this.name = name;
-    this.description = description;
-  }
+   constructor(id, name, description) {
+      this.id = id;
+      this.name = name;
+      this.description = description;
+   }
 }
 
 class Product {
 
-  constructor(id, name, image, price, description, quantity, categoryID) {
-    this.id = id;
-    this.name = name;
-    this.image = image;
-    this.price = price;
-    this.description = description;
-    this.quantity = quantity;
-    this.categoryID = categoryID;
-  }
+   constructor(id, name, image, price, description, quantity, categoryID) {
+      this.id = id;
+      this.name = name;
+      this.image = image;
+      this.price = price;
+      this.description = description;
+      this.quantity = quantity;
+      this.categoryID = categoryID;
+   }
 }
 
 class Order {
 
-  constructor(id, productID, quantity, status, userID) {
-    this.id = id;
-    this.productID = productID;
-    this.quantity = quantity;
-    this.status = status;
-    this.userID = userID;
-  }
+   constructor(id, productID, quantity, status, userID) {
+      this.id = id;
+      this.productID = productID;
+      this.quantity = quantity;
+      this.status = status;
+      this.userID = userID;
+   }
 }
 
 class Wishlist {
 
-  constructor(id, productID, userID) {
-    this.id = id;
-    this.productID = productID;
-    this.userID = userID;
-  }
+   constructor(id, productID, userID) {
+      this.id = id;
+      this.productID = productID;
+      this.userID = userID;
+   }
 }
 // #endregion
