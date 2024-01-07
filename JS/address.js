a636ae410606c1f147f962841c85961105fdea32
justomar21 <tarekes68@gmail.com>
2024-01-07 03:27:57 +0200

    Completed the functionality of Shopping Cart and Shipping Address pages , and completed their validation process

--- JS/address.js
+++ JS/address.js
@@ -1,10 +1,17 @@
-let fName = document.querySelector("#fName");
-let fNameVali = document.querySelector(".fNameVali");
-let fNameState = false;
+class ShippingInfo {
 
-let lName = document.querySelector("#lName");
-let lNameVali = document.querySelector(".lNameVali");
-let lNameState = false;
+   constructor(street, city, state, zipcode, country, phone, userID) {
+      this.street = street;
+      this.city = city;
+      this.state = state;
+      this.zipcode = zipcode;
+      this.country = country;
+      this.phone = phone;
+      this.userID = userID;
+   }
+}
+
+
 
 let street = document.querySelector("#address");
 let streetVali = document.querySelector(".addressVali");
@@ -32,9 +39,7 @@ let phoneState = false;
 
 let submit = document.querySelector(".shippingForm");
 
-fName.addEventListener("input" , validateFirstName);
-lName.addEventListener("input" , validateLastName);
-street.addEventListener("input",validateStreet);
+street.addEventListener("input", validateStreet);
 city.addEventListener("input", validateCity);
 state.addEventListener("input", validateState);
 zipcode.addEventListener("input", validateZipcode);
@@ -42,143 +47,221 @@ country.addEventListener("input", validateCountry);
 phone.addEventListener("input", validatePhone);
 submit.addEventListener("submit", submitFun);
 
+getData();
 
-function validateAll(){
-    validateFirstName();
-    validateLastName();
-    validateStreet();
-    validateCity();
-    validateState();
-    validateZipcode();
-    validateCountry();
-    validatePhone();
+
+function getData() {
+   if (sessionStorage.getItem("order") == null) {
+      alert("you cannot access this page directly, Please place an order first!");
+      location.assign("../shoppingCart.html");
+   }
+   if (sessionStorage.getItem("currentUserID") == null) {
+      alert("you are not logged in");
+      location.assign("../Store/index.html");
+   }
+   let userShips = JSON.parse(localStorage.getItem("shippingInfo")) || [];
+   let currentUserID = sessionStorage.getItem("currentUserID");
+
+   let singleUserShip = userShips.filter((user) => user.userID == currentUserID)[0];
+
+   street.value = singleUserShip.street || '';
+   city.value = singleUserShip.city || '';
+   state.value = singleUserShip.state || '';
+   zipcode.value = singleUserShip.zipcode || '';
+   country.value = singleUserShip.country || '';
+   phone.value = singleUserShip.phone || '';
+}
+
+function validateAll() {
+   validateStreet();
+   validateCity();
+   validateState();
+   validateZipcode();
+   validateCountry();
+   validatePhone();
 }
 
 
 
-function submitFun(e){
-    e.preventDefault();
-    validateAll();
+function submitFun(e) {
+   e.preventDefault();
+   validateAll();
+
+   let stateValidation = streetState && cityState && stateState && zipcodeState && countryState && phoneState;
+
+   if (stateValidation) {
+      let userShips = JSON.parse(localStorage.getItem("shippingInfo")) || [];
+      let singleUser = userShips.filter((ship) => ship.userID == sessionStorage.getItem("currentUserID"))[0];
+      if (singleUser) {
+         for(let user of userShips)
+         {
+            if(user.userID == sessionStorage.getItem("currentUserID"))
+            {
+               user.street = street.value;
+               user.city = city.value;
+               user.state = state.value;
+               user.zipcode = zipcode.value;
+               user.country = country.value;
+               user.phone = phone.value;
+               break;
+            }
+         }
+      }
+      else {
+         let singleUserShip = new ShippingInfo(street.value, city.value, state.value, zipcode.value, country.value, phone.value, sessionStorage.getItem("currentUserID"));
+         userShips.push(singleUserShip);
+      }
+
+      localStorage.setItem("shippingInfo", JSON.stringify(userShips));
+
+      
+      placeOrder();
+   }
+
+}
+
+function placeOrder() {
+
+   let tempOrder = JSON.parse(sessionStorage.getItem("order"));
+   tempOrder = tempOrder.filter((order)=>order.userID == sessionStorage.getItem("currentUserID"));
+   console.log(tempOrder);
+
+   orderNumber = tempOrder.length;
+
+   let shoppingCart = JSON.parse(localStorage.getItem("shoppingCart")) || [];
+   let products = JSON.parse(localStorage.getItem("product")) || [];
+   let count = 0;
+   for (let order of tempOrder) {
+      for (let prod of products) {
+         if (order.productID == prod.id) {
+            let orderPrice = tempOrder[count++].productPrice;
+            if (!(orderPrice == prod.price)) {
+               alert("A product price have been updated, redirecting the page");
+               location.assign("../shoppingCart");
+            }
+            console.log(Number(order.quantity) > Number(prod.quantity));
+            if (Number(order.quantity) > Number(prod.quantity)) {
+               alert(`the "${prod.name}" product has only ${prod.quantity} units, please lower your order quantity`);
+               location.assign("../shoppingCart");
+            }
+         }
+      }
+   }
+   let cartCount = shoppingCart.filter((item) => item.userID == sessionStorage.getItem("currentUserID"));
+   cartCount = cartCount.length;
+   if (cartCount != orderNumber) {
+      alert("A product may have been deleted, refreshing now");
+      location.assign("../shoppingCart");
+   }
+
+   for(let order of tempOrder)
+   {
+      for(let prod of products)
+      {
+         if(order.productID == prod.id)
+         {
+            prod.quantity = Number(prod.quantity) - Number(order.quantity);
+         }
+      }
+   }
+
+   for(let i = 0 ; i < shoppingCart.length ;i++)
+   {
+      if(shoppingCart[i].userID == sessionStorage.getItem("currentUserID"))
+      {
+         shoppingCart.splice(i--,1);
+      }
+   }
+
+   let orders = JSON.parse(localStorage.getItem("order")) || [];
+
+   for(let singleOrder of tempOrder)
+   {
+      orders.push(singleOrder);
+   }
+   localStorage.setItem("product", JSON.stringify(products));
+   localStorage.setItem("shoppingCart",JSON.stringify(shoppingCart));
+   sessionStorage.removeItem("order");
+   localStorage.setItem("order",JSON.stringify(orders));
+   location.assign("../shoppingCart.html");
 }
 
 
 
 
-function validateFirstName(){
-    let pattern = /^[a-zA-Z ]{2,30}$/;
+function validateStreet() {
+   let pattern = /^[#.0-9a-zA-Z\s,-]+$/;
 
-    if(pattern.test(fName.value))
-    {
-        fNameState = true;
-        fNameVali.innerHTML = ""
-    }
-    else
-    {
-        fNameState = false;
-        fNameVali.innerHTML = "Letters Only, 2 - 30 Character";
-    }
-}
-
-function validateLastName(){
-    let pattern = /^[a-zA-Z ]{2,30}$/;
-
-    if(pattern.test(lName.value))
-    {
-        lNameState = true;
-        lNameVali.innerHTML = "";
-    }
-    else
-    {
-        lNameState = false;
-        lNameVali.innerHTML = "Letters Only, 2 - 30 Character";
-    }
-}
-
-function validateStreet(){
-    let pattern = /^[#.0-9a-zA-Z\s,-]+$/;
-
-    if(pattern.test(street.value))
-    { 
-        streetState = true ;
-        streetVali.innerHTML = "";
-    }
-    else
-    {
-        streetState = false;
-        streetVali.innerHTML = "No Special Characters Allowed";
-    }
+   if (pattern.test(street.value)) {
+      streetState = true;
+      streetVali.innerHTML = "";
+   }
+   else {
+      streetState = false;
+      streetVali.innerHTML = "No Special Characters Allowed";
+   }
 }
 
 function validateCity() {
-    let pattern = /^[0-9a-zA-Z\s]+$/;
+   let pattern = /^[0-9a-zA-Z\s]+$/;
 
-    if(pattern.test(city.value))
-    { 
-        cityState = true ;
-        cityVali.innerHTML = "";
-    }
-    else
-    {
-        cityState = false;
-        cityVali.innerHTML = "Letters and Numbers Only";
-    }
+   if (pattern.test(city.value)) {
+      cityState = true;
+      cityVali.innerHTML = "";
+   }
+   else {
+      cityState = false;
+      cityVali.innerHTML = "Letters and Numbers Only";
+   }
 }
 
 function validateState() {
-    let pattern = /^[0-9a-zA-Z\s]+$/;
+   let pattern = /^[0-9a-zA-Z\s]+$/;
 
-    if(pattern.test(state.value))
-    { 
-        stateState = true ;
-        stateVali.innerHTML = "";
-    }
-    else
-    {
-        stateState = false;
-        stateVali.innerHTML = "Letters and Numbers Only";
-    }
+   if (pattern.test(state.value)) {
+      stateState = true;
+      stateVali.innerHTML = "";
+   }
+   else {
+      stateState = false;
+      stateVali.innerHTML = "Letters and Numbers Only";
+   }
 }
 
 function validateZipcode() {
-    let pattern = /^[0-9]+$/;
+   let pattern = /^[0-9]+$/;
 
-    
-    if(pattern.test(zipcode.value))
-    { 
-        zipcodeState = true ;
-        zipcodeVali.innerHTML = "";
-    }
-    else
-    {
-        zipcodeState = false;
-        zipcodeVali.innerHTML = "Numbers Only";
-    }
+
+   if (pattern.test(zipcode.value)) {
+      zipcodeState = true;
+      zipcodeVali.innerHTML = "";
+   }
+   else {
+      zipcodeState = false;
+      zipcodeVali.innerHTML = "Numbers Only";
+   }
 }
 
-function validateCountry(){
-    if(country.value =='')
-    {
-        countryState = true;
-        countryVali.innerHTML = "Select A Country";
-    }
-    else
-    {
-        countryState = false;
-        countryVali.innerHTML = "";
-    }
+function validateCountry() {
+   if (country.value == '') {
+      countryState = false;
+      countryVali.innerHTML = "Select A Country";
+   }
+   else {
+      countryState = true;
+      countryVali.innerHTML = "";
+   }
 }
 
-function validatePhone(){
-    let pattern = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/gmi;
+function validatePhone() {
+   let pattern = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/gmi;
 
-    if(pattern.test(phone.value))
-    {
-        phoneState = true;
-        phoneVali.innerHTML ="";
-    }
-    else
-    {
-        phoneState = false;
-        phoneVali.innerHTML = "Enter A Valid Number";
-    }
+   if (pattern.test(phone.value)) {
+      phoneState = true;
+      phoneVali.innerHTML = "";
+   }
+   else {
+      phoneState = false;
+      phoneVali.innerHTML = "Enter A Valid Number";
+   }
 }
--- JS/shoppingCart.js
+++ JS/shoppingCart.js
@@ -1,3 +1,17 @@
+class Order {
+
+   constructor(id, productID, productName, productPrice, productImage, quantity, total, status, userID) {
+      this.id = id;
+      this.productID = productID;
+      this.productName = productName;
+      this.productPrice = productPrice;
+      this.productImage = productImage;
+      this.quantity = quantity;
+      this.total = total;
+      this.status = status;
+      this.userID = userID;
+   }
+}
 let table = document.querySelector("table");
 let cartCheck = document.querySelector(".emptyCart");
 
@@ -16,8 +30,9 @@ let discount = document.querySelector(".pricesDiscount");
 let shipping = document.querySelector(".pricesShipping");
 let totalTotal = document.querySelector(".pricesTotal");
 let orderBtn = document.querySelector(".order");
+orderBtn.addEventListener("click", placeTempOrder);
 
-
+updatePriceAll();
 for (let ele in sub) {
 
    sub[ele].addEventListener("click", subtract);
@@ -39,12 +54,10 @@ function getData() {
 
 
    let shoppingCart = JSON.parse(localStorage.getItem("shoppingCart")) || [];
-   for (let entry of shoppingCart) 
-   {
-      for (let product of products)
-      {
+   for (let entry of shoppingCart) {
+      for (let product of products) {
          if (entry.userID == currentUser && product.id == entry.productID) {
-            tableRowBuilder(entry , product);
+            tableRowBuilder(entry, product);
          }
       }
 
@@ -54,221 +67,267 @@ function getData() {
 
 
 
-   /* Code Builder Structure
-   
-       
-   
-         <tr>
-            <td class="itemDetails">
-               <img src="assets/products/electronic1.jpg" alt="">
-               <span class="itemTitle">a random laptop that probably sucks</span>
-            </td>
-            <td class="price">99.99$</td>
-            <td class="qty">
-               <button class="subtract">-</button>
-               <input class="itemQuantity" value="1">
-               <button class="add">+</button>
-            </td>
-            <td class="subtotal">99.99$</td>
-            <td class="deleteItem">X</td>
-            <td display: none>productID<td>
-         </tr>
-   
-   */
+/* Code Builder Structure
+ 
+    
+ 
+      <tr>
+         <td class="itemDetails">
+            <img src="assets/products/electronic1.jpg" alt="">
+            <span class="itemTitle">a random laptop that probably sucks</span>
+         </td>
+         <td class="price">99.99$</td>
+         <td class="qty">
+            <button class="subtract">-</button>
+            <input class="itemQuantity" value="1">
+            <button class="add">+</button>
+         </td>
+         <td class="subtotal">99.99$</td>
+         <td class="deleteItem">X</td>
+         <td display: none>productID<td>
+      </tr>
+ 
+*/
 
-   function tableRowBuilder(entry , product) {
+function tableRowBuilder(entry, product) {
 
-      //#region getting Data
-
-
-
-      //#endregion
-
-      // #region Creating the title and image TD (td => img , span)
-
-      let detailsTD = document.createElement("td");
-      detailsTD.classList.add("itemDetails");
-
-      let img = document.createElement("img");
-      img.src = `${product.image}` // it will be dynamic here
-
-      let span = document.createElement("span");
-      span.innerHTML = `${product.name}` // it will be dynamic here
-      span.classList.add("itemTitle");
-
-      detailsTD.appendChild(img);
-      detailsTD.appendChild(span);
-
-      // #endregion
-
-      // #region Creating a Single Item Price TD (td => price)
-
-      let priceTD = document.createElement("td");
-      priceTD.classList.add("price");
-      priceTD.innerHTML = `${product.price}$`; // it will be dynamic here
-
-      //#endregion
-
-      // #region Creating the quantity input with its buttons 
-      // (td => subBtn , input , addBtn)
-
-      let qtyTD = document.createElement("td");
-      qtyTD.classList.add("qty");
-
-      let subBtn = document.createElement("button");
-      subBtn.classList.add("subtract");
-      subBtn.innerHTML = `-`;
-
-      let quantityInput = document.createElement("input");
-      quantityInput.classList.add("itemQuantity");
-      quantityInput.value = entry.quantity; // this will be dynamic
-
-      let addBtn = document.createElement("button");
-      addBtn.classList.add("add");
-      addBtn.innerHTML = `+`;
-
-      qtyTD.appendChild(subBtn);
-      qtyTD.appendChild(document.createTextNode(" "));
-      qtyTD.appendChild(quantityInput);
-      qtyTD.appendChild(document.createTextNode(" "));
-      qtyTD.appendChild(addBtn);
-      /* this is important because for some reason
-      document create element does it like this
-      <ele></ele><ele></ele> 
-      instead of 
-      <ele></ele> <ele></ele>
-       THAT LITTLE SPACE BETWEEN IS IMPORTANT */
-
-
-      // #endregion
-
-      // #region Creating the subtotal for a single item and delete button
-      // (td => subtotal , td => delete button)
-
-      let subtotalTD = document.createElement("td");
-      subtotalTD.classList.add("subtotal");
-      subtotalTD.innerHTML = `${entry.quantity * product.price}$`; // this will be dynamic
-
-      let deleteTD = document.createElement("td");
-      deleteTD.classList.add("deleteItem");
-      deleteTD.innerHTML = `X`;
-
-      let entryIDTD = document.createElement("td")
-      entryIDTD.innerHTML = entry.id;
-      entryIDTD.setAttribute("style","display:none");
-
-      // #endregion
-
-      // #region Product ID
+   //#region getting Data
 
 
 
-      // #endregion
+   //#endregion
 
-      // #region Appending all td to tr and table
-      // (td.All => tr => table)
+   // #region Creating the title and image TD (td => img , span)
 
-      let trElement = document.createElement("tr");
-      trElement.appendChild(detailsTD);
-      trElement.appendChild(priceTD);
-      trElement.appendChild(qtyTD);
-      trElement.appendChild(subtotalTD);
-      trElement.appendChild(deleteTD);
-      trElement.appendChild(entryIDTD);
-      table.firstElementChild.appendChild(trElement);
+   let detailsTD = document.createElement("td");
+   detailsTD.classList.add("itemDetails");
 
-      // #endregion
+   let img = document.createElement("img");
+   img.src = `${product.image}` // it will be dynamic here
 
-   }
+   let span = document.createElement("span");
+   span.innerHTML = `${product.name}` // it will be dynamic here
+   span.classList.add("itemTitle");
+
+   detailsTD.appendChild(img);
+   detailsTD.appendChild(span);
+
+   // #endregion
+
+   // #region Creating a Single Item Price TD (td => price)
+
+   let priceTD = document.createElement("td");
+   priceTD.classList.add("price");
+   priceTD.innerHTML = `${product.price}$`; // it will be dynamic here
+
+   //#endregion
+
+   // #region Creating the quantity input with its buttons 
+   // (td => subBtn , input , addBtn)
+
+   let qtyTD = document.createElement("td");
+   qtyTD.classList.add("qty");
+
+   let subBtn = document.createElement("button");
+   subBtn.classList.add("subtract");
+   subBtn.innerHTML = `-`;
+
+   let quantityInput = document.createElement("input");
+   quantityInput.classList.add("itemQuantity");
+   quantityInput.value = entry.quantity; // this will be dynamic
+
+   let addBtn = document.createElement("button");
+   addBtn.classList.add("add");
+   addBtn.innerHTML = `+`;
+
+   qtyTD.appendChild(subBtn);
+   qtyTD.appendChild(document.createTextNode(" "));
+   qtyTD.appendChild(quantityInput);
+   qtyTD.appendChild(document.createTextNode(" "));
+   qtyTD.appendChild(addBtn);
+   /* this is important because for some reason
+   document create element does it like this
+   <ele></ele><ele></ele> 
+   instead of 
+   <ele></ele> <ele></ele>
+    THAT LITTLE SPACE BETWEEN IS IMPORTANT */
 
 
-   function updatePrice(itemQuantity, singleItem, totalItem) {
-      let singleItemValue = parseFloat(singleItem.innerText);
-      let quantity = itemQuantity.value;
-      let total = singleItemValue * quantity;
-      total = total.toFixed(2);
-      totalItem.innerText = `${total}$`;
-      updatePriceAll();
-   }
+   // #endregion
 
-   function updatePriceAll() {
-      let total = 0;
-      itemSubtotal.forEach((x) => {
-         total += parseFloat(x.innerText);
-      })
-      totalTotal.innerHTML = `${total.toFixed(2)}$`; 
+   // #region Creating the subtotal for a single item and delete button
+   // (td => subtotal , td => delete button)
 
-      if (total > 0) {
-         cartCheck.innerHTML = "";
-         orderBtn.style.display = "block";
-         totalTotal.innerHTML = `${total.toFixed(2)}$`;
-      }
-      else {
-         cartCheck.innerHTML = "No items in the cart to order";
-         orderBtn.style.display = "none";
-         totalTotal.innerHTML = `0.00$`;
-      }
+   let subtotalTD = document.createElement("td");
+   subtotalTD.classList.add("subtotal");
+   subtotalTD.innerHTML = `${entry.quantity * product.price}$`; // this will be dynamic
 
-   }
+   let deleteTD = document.createElement("td");
+   deleteTD.classList.add("deleteItem");
+   deleteTD.innerHTML = `X`;
 
-   function addition(e) {
-      let entryID = e.target.parentElement.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML;
-      let itemQuantity = e.target.previousElementSibling
-      if (itemQuantity.value < 10) {
-         itemQuantity.value++;
-      }
-      let shoppingCart = JSON.parse(localStorage.getItem("shoppingCart")) || [];
-      for(let entry of shoppingCart)
-      {
-         if(entry.id == entryID)
-         {
-            entry.quantity = itemQuantity.value;
-         }
-      }
-      localStorage.setItem("shoppingCart",JSON.stringify(shoppingCart));
-      let singleItem = e.target.parentElement.previousElementSibling;
-      let totalItem = e.target.parentElement.nextElementSibling;
-      updatePrice(itemQuantity, singleItem, totalItem);
-   }
+   let entryIDTD = document.createElement("td")
+   entryIDTD.innerHTML = entry.id;
+   entryIDTD.setAttribute("style", "display:none");
 
-   function subtract(e) {
-      let entryID = e.target.parentElement.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML;
-      let itemQuantity = e.target.nextElementSibling
-      if (itemQuantity.value > 1) {
-         itemQuantity.value--;
-      }
-      let shoppingCart = JSON.parse(localStorage.getItem("shoppingCart")) || [];
-      for(let entry of shoppingCart)
-      {
-         if(entry.id == entryID)
-         {
-            entry.quantity = itemQuantity.value;
-         }
-      }
-      localStorage.setItem("shoppingCart",JSON.stringify(shoppingCart));
-      let singleItem = e.target.parentElement.previousElementSibling;
-      let totalItem = e.target.parentElement.nextElementSibling;
-      updatePrice(itemQuantity, singleItem, totalItem);
-   }
+   // #endregion
 
-   function deleteProduct(e) {
-      // Remember to remove data from local storage
-      let shoppingCart = JSON.parse(localStorage.getItem("shoppingCart")) || [];
-      let entryID = Number(e.target.nextElementSibling.innerHTML);
+   // #region Product ID
 
-      for(let entry in shoppingCart)
-      {
-         if(shoppingCart[entry].id == entryID)
-         {
-            shoppingCart.splice(entry,1);
-         }
-      }
-      localStorage.setItem("shoppingCart",JSON.stringify(shoppingCart));
-      let total = parseFloat(totalTotal.innerText);
-      console.log(e.target.previousElementSibling);
-      total -= parseFloat(e.target.previousElementSibling.innerText);
+
+
+   // #endregion
+
+   // #region Appending all td to tr and table
+   // (td.All => tr => table)
+
+   let trElement = document.createElement("tr");
+   trElement.appendChild(detailsTD);
+   trElement.appendChild(priceTD);
+   trElement.appendChild(qtyTD);
+   trElement.appendChild(subtotalTD);
+   trElement.appendChild(deleteTD);
+   trElement.appendChild(entryIDTD);
+   table.firstElementChild.appendChild(trElement);
+
+   // #endregion
+
+}
+
+
+function updatePrice(itemQuantity, singleItem, totalItem) {
+   let singleItemValue = parseFloat(singleItem.innerText);
+   let quantity = itemQuantity.value;
+   let total = singleItemValue * quantity;
+   total = total.toFixed(2);
+   totalItem.innerText = `${total}$`;
+   updatePriceAll();
+}
+
+function updatePriceAll() {
+   let total = 0;
+   itemSubtotal.forEach((x) => {
+      total += parseFloat(x.innerText);
+   })
+   totalTotal.innerHTML = `${total.toFixed(2)}$`;
+
+   if (total > 0) {
+      cartCheck.innerHTML = "";
+      orderBtn.style.display = "block";
       totalTotal.innerHTML = `${total.toFixed(2)}$`;
-      e.target.parentElement.remove();
-      itemSubtotal = document.querySelectorAll(".subtotal");
-      updatePriceAll();
    }
+   else {
+      cartCheck.innerHTML = "No items in the cart to order";
+      orderBtn.style.display = "none";
+      totalTotal.innerHTML = `0.00$`;
+   }
+
+}
+
+function addition(e) {
+   let entryID = e.target.parentElement.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML;
+   let itemQuantity = e.target.previousElementSibling
+   if (itemQuantity.value < 10) {
+      itemQuantity.value++;
+   }
+   let shoppingCart = JSON.parse(localStorage.getItem("shoppingCart")) || [];
+   for (let entry of shoppingCart) {
+      if (entry.id == entryID) {
+         entry.quantity = itemQuantity.value;
+      }
+   }
+   localStorage.setItem("shoppingCart", JSON.stringify(shoppingCart));
+   let singleItem = e.target.parentElement.previousElementSibling;
+   let totalItem = e.target.parentElement.nextElementSibling;
+   updatePrice(itemQuantity, singleItem, totalItem);
+}
+
+function subtract(e) {
+   let entryID = e.target.parentElement.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML;
+   let itemQuantity = e.target.nextElementSibling
+   if (itemQuantity.value > 1) {
+      itemQuantity.value--;
+   }
+   let shoppingCart = JSON.parse(localStorage.getItem("shoppingCart")) || [];
+   for (let entry of shoppingCart) {
+      if (entry.id == entryID) {
+         entry.quantity = itemQuantity.value;
+      }
+   }
+   localStorage.setItem("shoppingCart", JSON.stringify(shoppingCart));
+   let singleItem = e.target.parentElement.previousElementSibling;
+   let totalItem = e.target.parentElement.nextElementSibling;
+   updatePrice(itemQuantity, singleItem, totalItem);
+}
+
+function deleteProduct(e) {
+   // Remember to remove data from local storage
+   let shoppingCart = JSON.parse(localStorage.getItem("shoppingCart")) || [];
+   let entryID = Number(e.target.nextElementSibling.innerHTML);
+
+   for (let entry in shoppingCart) {
+      if (shoppingCart[entry].id == entryID) {
+         shoppingCart.splice(entry, 1);
+      }
+   }
+   localStorage.setItem("shoppingCart", JSON.stringify(shoppingCart));
+   let total = parseFloat(totalTotal.innerText);
+   console.log(e.target.previousElementSibling);
+   total -= parseFloat(e.target.previousElementSibling.innerText);
+   totalTotal.innerHTML = `${total.toFixed(2)}$`;
+   e.target.parentElement.remove();
+   itemSubtotal = document.querySelectorAll(".subtotal");
+   updatePriceAll();
+}
+
+
+function placeTempOrder() {
+   
+   let allTrs = document.querySelectorAll("tr");
+
+   orderNumber = allTrs.length - 1;
+
+   let shoppingCart = JSON.parse(localStorage.getItem("shoppingCart")) || [];
+   let products = JSON.parse(localStorage.getItem("product")) || [];
+   let count = 1;
+   for (let cart of shoppingCart) {
+      for (let prod of products) {
+         if (cart.productID == prod.id) {
+            let cartPrice = parseFloat(allTrs[count++].childNodes[1].innerHTML);
+            if (!(cartPrice == prod.price)) {
+               alert("A product price have been updated, reloading the page");
+               location.reload();
+            }
+            console.log(Number(cart.quantity) > Number(prod.quantity));
+            if (Number(cart.quantity) > Number(prod.quantity)) {
+               alert(`the "${prod.name}" product has only ${prod.quantity} units, please lower your order quantity`);
+               return;
+            }
+         }
+      }
+   }
+   let cartCount = shoppingCart.filter((item) => item.userID == sessionStorage.getItem("currentUserID"));
+   cartCount = cartCount.length;
+   if (cartCount != orderNumber) {
+      alert("A product may have been deleted, refreshing now");
+      location.reload();
+   }
+
+   let userOrder = [];
+   for (let cart of shoppingCart) {
+      for (let prod of products) {
+         if (cart.productID == prod.id) {
+            let orderID = localStorage.getItem("orderID") || 0;
+            localStorage.setItem("orderID", Number(orderID) + 1);
+            let orderItem = new Order(orderID, cart.productID, prod.name, prod.price, prod.image, cart.quantity, Number(prod.price) * Number(cart.quantity), "Pending", sessionStorage.getItem("currentUserID"));
+            userOrder.push(orderItem);
+         }
+      }
+   }
+
+   console.log(userOrder);
+   sessionStorage.setItem("order",JSON.stringify(userOrder));
+   location.assign("../address.html");
+}
+
+
